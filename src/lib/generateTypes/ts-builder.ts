bbff346444c08423dec13ecef51bea65b943c769
Raimund Canzler <git@rai-canzler.de>
2024-01-31 10:02:27 +0100

    feat: add option to treat required as non-nullable for typescript
    feat: add treatRequiredAsNonNullable option for typescript

--- src/lib/generateTypes/ts-builder.ts
+++ src/lib/generateTypes/ts-builder.ts
@@ -5,6 +5,7 @@ export class TypeBuilder {
     private collections: Collections,
     private useIntersectionTypes: boolean,
     private sdk11: boolean,
+    private treatRequiredAsNonNull: boolean,
   ) {}
 
   build() {
@@ -97,7 +98,10 @@ export class TypeBuilder {
       }
     }
 
-    if (field.schema?.is_nullable) {
+    if (
+      field.schema?.is_nullable &&
+      !(this.treatRequiredAsNonNull && field.meta?.required)
+    ) {
       if (field.relation && this.useIntersectionTypes) {
         type = `(${type}) | null`;
       } else {
